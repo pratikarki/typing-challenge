806ae6141cdb3e98d88575198876b159a471925f
Pratik Karki <pratikkarki5@gmail.com>
2021-07-22 09:03:52 +0545

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { lorem } from 'faker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'typing-challenge';
+  randomText = lorem.sentence();
+  enteredText = '';
+  userTime = '';
+  displayTime = '';
+  displayWord = '';
+  isSuccess = false;
+
+  onInputChange(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    // console.log(this.randomText);
+
+    if (this.enteredText === '' && this.displayWord !== 'Yout are a Cheater!') {
+      this.userTime = Date.now().toString();
+    }
+
+    if (value === this.randomText) {
+      this.isSuccess = true;
+      if (this.displayWord === 'Yout are a Cheater!' || this.displayWord === 'Once a Cheater, Always a cheater!') {
+        this.displayWord = 'Once a Cheater, Always a cheater!';
+      }
+      else {
+        this.displayTime = this.convertMillisToMinutesAndSeconds(Date.now() - parseInt(this.userTime));
+        this.displayWord = (this.displayTime === '0:00')? 'Yout are a Cheater!' : 'Well Done!';
+      }
+    }
+    else {
+      this.isSuccess = false;
+    }
+    
+    this.enteredText = value;    
+  }
+
+  compareLetters(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) return 'pending';
+    return (randomLetter === enteredLetter)? 'correct': 'incorrect';
+  }
+
+  convertMillisToMinutesAndSeconds(millis: number) {
+    let minutes = Math.floor(millis / 60000);
+    let seconds = ((millis % 60000) / 1000).toFixed(0);
+    return minutes + ":" + (parseInt(seconds) < 10 ? '0' : '') + seconds;
+  }
 }
